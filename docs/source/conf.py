029b1d3ba59e04c4384140b9442b5ee6f358fdb4
rainlok <webmaster@itmader.ovh>
2021-12-27 09:55:55 +0100

    minimal update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,5 +33,10 @@ html_theme = 'sphinx_rtd_theme'
 html_logo = 'images/logo/hacker.webp'
 html_favicon = 'images/favicon/favicon.ico'
 
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
